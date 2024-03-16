62e467565a0b53aecf7ffc1774354927cb6ce9b1
Yuting <liyuting2004@gmail.com>
2024-03-17 00:45:53 +0800

    complete question 3

--- hw/hw1/3.c
+++ hw/hw1/3.c
@@ -43,3 +43,33 @@ int main()
         }
     }
 }
+
+/**
+ * Execute the upper program can get result as followed:
+ * 
+ * Loop 1, pid: 32324
+ * Loop 1, pid: 32325, child of 32324
+ * Loop 2, pid: 32325
+ * Loop 2, pid: 32324
+ * Loop 2, pid: 32327, child of 32325
+ * Loop 2, pid: 32326, child of 32324
+ * 
+ * It can be easily to notice that there are 4 pid: 32324, 32325, 32326 and 32327,
+ * which indecate that total 4 process is created.
+ * 
+ * The process tree is as followed:
+ *  32324─┬─32325───32327
+ *        └─32326
+ * 
+ * In loop 1, the parent process(pid: 32324) creates a child process(pid: 32325),
+ * in the fork process, all the codes and variables are copyed, 
+ * which indate that in child process(pid:32325), the variables i will be 0.
+ * 
+ * In loop 2, the parent process(pid: 32324) creates a new child process just 
+ * like loop 1, whose pid is 32326.
+ * The child process(pid: 32325) in loop 1 creates its own child process in loop 2,
+ * whose pid is 32327.
+ * 
+ * After fork process of loop 2, now all four process has the same i, which will be 1,
+ * that will end the loop in all four process and that is the end of the program.
+*/
