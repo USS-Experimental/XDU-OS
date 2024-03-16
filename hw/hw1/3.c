4cc78c505344cff15bf0c6f7ba0439a72c63622b
Yuting <liyuting2004@gmail.com>
2024-03-16 23:39:41 +0800

    complete the question part of the question 3

---
+++ hw/hw1/3.c
@@ -0,0 +1,12 @@
+/**
+ * How many processes will be created after running the program listed below?
+ * 
+ * int main()
+ * {
+ *     pid_t pid;
+ *     for (int i = 0; i < 2; i++)
+ *     {
+ *         pid = fork();
+ *     }
+ * }
+*/
