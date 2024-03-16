8c4a841c1e750b2a3e0cd710ae59ae417018cb53
Yuting <liyuting2004@gmail.com>
2024-03-17 00:29:06 +0800

    complete code part of question 3

--- hw/hw1/3.c
+++ hw/hw1/3.c
@@ -10,3 +10,36 @@
  *     }
  * }
 */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    __pid_t pid;
+
+    for (size_t i = 0; i < 2; i++)
+    {
+        pid = fork();
+
+        if (i == 1) // must sleep in loop 2 to get correct pid
+        {
+            sleep(1);
+        }
+
+        if (pid < 0) // fork failed
+        {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid == 0) // in chlid process
+        {
+            printf("Loop %zu, pid: %d, child of %d\n", i + 1, getpid(), getppid());
+        }
+        else // in parent process
+        {
+            printf("Loop %zu, pid: %d\n", i + 1, getpid());
+        }
+    }
+}
