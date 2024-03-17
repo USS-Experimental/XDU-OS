2feef1a50197561cd979ca33a2a0a9d98bb74680
Yuting <liyuting2004@gmail.com>
2024-03-17 15:31:18 +0800

    complete code part of q.7

--- hw/hw1/7.c
+++ hw/hw1/7.c
@@ -10,3 +10,21 @@
  *     printf("I love OS\n");
  * }
 */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    __pid_t pid;
+    
+    pid = fork();
+
+    if (pid == 0)
+    {
+        printf("pid_t: %d I love OS\n", pid);
+    }
+    
+    printf("pid_t: %d I love OS\n", pid);
+}
