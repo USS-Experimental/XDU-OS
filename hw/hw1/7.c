5fa7a69aaa4232ae8745c56883749aab53c20f66
Yuting <liyuting2004@gmail.com>
2024-03-17 15:27:10 +0800

    add the question part of question 7

---
+++ hw/hw1/7.c
@@ -0,0 +1,12 @@
+/**
+ * How many "I love OS\n" will be printed in the following program?
+ * 
+ * int main()
+ * {
+ *     pid_t pid;
+ *     pid = fork();
+ *     if (pid == 0)
+ *         printf("I love OS\n");
+ *     printf("I love OS\n");
+ * }
+*/
