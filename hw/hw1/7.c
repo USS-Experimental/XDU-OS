b5aa0e3e5808c5b852451acfa971fe207baf1239
Yuting <liyuting2004@gmail.com>
2024-03-17 15:36:56 +0800

    finish q.7

--- hw/hw1/7.c
+++ hw/hw1/7.c
@@ -23,8 +23,20 @@ int main()
 
     if (pid == 0)
     {
-        printf("pid_t: %d I love OS\n", pid);
+        printf("pid: %d pid_t: %d I love OS\n", getpid(), pid);
     }
     
-    printf("pid_t: %d I love OS\n", pid);
+    printf("pid: %d pid_t: %d I love OS\n", getpid(), pid);
 }
+
+/**
+ * Three "I love OS\n" will be print after execute the upper program.
+ * 
+ * pid: 5237 pid_t: 5238 I love OS
+ * pid: 5238 pid_t: 0 I love OS
+ * pid: 5238 pid_t: 0 I love OS
+ * 
+ * In the parent process, the return value of fork() will be the pid
+ * of the child process.
+ * In the child process, the return value of fork() will be 0 
+*/
